cb531d17c46960828eb9aa6c423dd16675c67bc9
wangshane <wzy545@vip.qq.com>
2016-12-04 17:16:14 +0800

    git dif

--- Chapter_10/Guess_number.c
+++ Chapter_10/Guess_number.c
@@ -4,55 +4,57 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_NUMBER 100
+#define MAX_NUMBER 10
 
 void initialize_number_generator(void);
 void read_guesses(int secret_number);
 int new_secret_number(void);
 
-int main (void)
+int main(void)
 {
-    char command;
-    int secret_number;
+	char command;
+	int secret_number;
 
-    printf("Guess the secret number between 1 and %d.\n\n", MAX_NUMBER);
-    initialize_number_generator();
+	printf("Guess the secret number between 1 and %d.\n\n", MAX_NUMBER);
+	initialize_number_generator();
 
-    do {
-        secret_number = new_secret_number();
-        printf("A new number has been chosen.\n");
-        read_guesses(secret_number);
-        printf("Play again? (Y/N)\n");
-        scanf("%c", &command);
-    } while (command == 'Y' || command == 'y');
+	do {
+		secret_number = new_secret_number();
+		printf("A new number has been chosen.\n");
+		read_guesses(secret_number);
+		printf("Play again? (Y/N) ");
+		scanf(" %c", &command);			//scanf("%c", &command);      '\'&'n' is char type, " "in front %c is needed
+		printf("\n");
+	} while (command == 'Y' || command == 'y');
 
-    return 0;
+	return 0;
 }
 
 void initialize_number_generator(void)
 {
-    srand((unsigned) time(NULL));
+	srand((unsigned)time(NULL));
 }
 
 int new_secret_number(void)
 {
-    return rand() % MAX_NUMBER + 1;
+	return rand() % MAX_NUMBER + 1;
 }
 
 void read_guesses(int secret_number)
 {
-    int guess, counter;
+	int guess, counter = 0;
 
-    while (1) {
-        counter++;
-        printf("Enter guess: ");
-        scanf("%d", &guess);
-        if (guess == secret_number) {
-            printf("You won in %d guesses!\n", counter);
-            return;
-        } else if (guess < secret_number)
-            printf("Too low; try again.\n");
-        else
-            printf("Too high; try again\n");
-    }
+	while (1) {   //for (;;)
+		counter++;
+		printf("Enter guess: ");
+		scanf("%d", &guess);
+		if (guess == secret_number) {
+			printf("You won in %d guesses!\n\n", counter);
+			break;          //return
+		}
+		else if (guess < secret_number)
+			printf("Too low; try again.\n");
+		else
+			printf("Too high; try again\n");
+	}
 }
