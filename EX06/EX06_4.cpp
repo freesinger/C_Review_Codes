05b036e8ec58e8a75a79cf1a66c4212f63dbab28
ShaneWang <wzy545@vip.qq.com>
2017-03-29 16:02:01 +0800

    Revert "fix"
    This reverts commit 212e7a48c9af6bff61e60519fdc8d5b6c1c69e8d.

--- EX06/EX06_4.cpp
+++ EX06/EX06_4.cpp
@@ -1,56 +1,50 @@
 #include <stdio.h>
 
-#define N 4
+#define N 2
 
 int main(void)
 {
-	int matrix[N][N], matrix2[N][N], max = 0;
+	int matrix[N][N], *x, *y, *max = 0, *temp;
 	int row, clo, pos_x, pos_y;
 
 	printf("Enter %d * %d matrix: ", N, N);
 
 	for (row = 0; row < N; row++)
 		for (clo = 0; clo < N; clo++)
-			scanf("%d", &matrix[row][clo]);
+			scanf("%d", matrix[row][clo]);
 
 	// convert matrix
 	for (row = 0; row < N; row++)
-		for (clo = 0; clo < N; clo++)
-			matrix2[clo][row] = matrix[row][clo];
+		for (clo = 0; clo < N; clo++) {
+			*x = matrix[row][clo];
+			*y = matrix[clo][row];
+			if (row != clo) {
+				temp = x;
+				x = y;
+				y = temp;
+			}
+		}
 
 	// find MAX number
 	for (row = 0; row < N; row++)
 		for (clo = 0; clo < N; clo++) {
-			if (matrix2[row][clo] > max) {
-				max = matrix2[row][clo];
+			if (matrix[row][clo] > *max) {
+				*max = matrix[row][clo];
 				pos_x = row;
 				pos_y = clo;
 			}
 		}
-
+	
 	// print the converted matrix
-	printf("Converted matrix: \n");
-	for (row = 0; row < N; row++) {
-		for (clo = 0; clo < N; clo++)
-			printf("%3d ", matrix2[row][clo]);
-		printf("\n");
-	}
+	for (row = 0; row < N; row++)
+		for (clo = 0; clo < N; clo++) {
+			printf("%d ", matrix[row][clo]);
+			if (clo == N - 1)
+				printf("\n");
+		}
 
-	printf("Max nummber is: %d\n", max);
-	printf("Position(row, clo): (%d, %d)\n", pos_x + 1, pos_y + 1);
+	printf("Max nummber is: %d\n", *max);
+	printf("Position: (%d, %d)\n", pos_x + 1, pos_y + 1);
 
-	return 0;
+    return 0;
 }
-
-///mark:
-// convert matrix
-/* for (row = 0; row < N; row++)
-for (clo = 0; clo < N; clo++) {
-*x = matrix[row][clo];
-*y = matrix[clo][row];
-if (row != clo) {
-temp = x;
-x = y;
-y = temp;
-}
-} */
