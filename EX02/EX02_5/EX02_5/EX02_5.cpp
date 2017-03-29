fbe9378ee20d814af62f98c8b936f6b1dce0d617
wangshane <wzy545@vip.qq.com>
2017-03-29 21:59:08 +0800

    fix

--- EX02/EX02_5/EX02_5/EX02_5.cpp
+++ EX02/EX02_5/EX02_5/EX02_5.cpp
@@ -1,38 +1,15 @@
 #include "stdafx.h"
-#include <string.h>
-#include <ctype.h>
 
-int multyten(int i, int counter)
-{
-	while (counter > 0)
-		i *= 10;
-
-	return i;
-}
 
 int main(void)
 {
-	char *str = "a123x456s17960?302tab5876";
-	int len = strlen(str),temp = 0;
-	int res[100], j = 0, count = 0;
+	int a, b, c;
+	printf("Enter two integer: ");
+	scanf("%d%d", &a, &b);
 
-	for (int i = len - 1; i >= 0; i--) {
-		if (*(str + i) >= '0' && *(str + i) <= '9') {     //if (isdigit(*(str + i))) {
-			temp += multyten(*(str + i) - 48, count);
-			count++;
-		}
-		else {
-			count = 0;
-			res[j++] = temp;
-			temp = 0;
-		}
-	}
+	c = a / 10 * 1000 + b / 10 * 100 + a % 10 * 10 + b % 10;
 
-	for (int i = 0; i < 100; i++) {
-		if (res[i] != 0)
-			printf("%d ", res[i]);
-	}
-	printf("\n");
+	printf("Value of c: %d\n", c);
 
 	return 0;
 }
