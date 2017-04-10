c873746555fb62a7f205a9e0b7ec04d2bc17fb9a
wangshane <wzy545@vip.qq.com>
2017-04-10 21:37:23 +0800

    fiX

--- Chapter_20/XOR/XOR.c
+++ Chapter_20/XOR/XOR.c
@@ -1,4 +1,4 @@
-/* Perform XOR encryption */
+/* Perform simple encryption */
 
 #include <ctype.h>
 #include <stdio.h>
@@ -7,17 +7,21 @@
 
 int main(void)
 {
-    int ori_char, res_char;
+	int ori_char, res_char;
 
-    while ((ori_char = getchar()) != EOF)
-    {
-        res_char = ori_char ^ KEY;
-        if (isprint(ori_char) && isprint(res_char))
-        //isprint() check out whether output can be put out
-            printf("%c", res_char); //putchar(res_char);
-        else 
-            printf("%c", ori_char); //putchar(ori_char);
-    }
+	while ((ori_char = getchar()) != EOF)
+	{
+		if (isdigit(ori_char))
+			res_char = ori_char + KEY; // reprogramme won't decode digit
+		else 
+			res_char = ori_char ^ KEY; // XOR encryption can be reversed
 
-    return 0;
+		if (isprint(ori_char) && isprint(res_char))
+			// isprint() check out whether output can be put out
+			printf("%c", res_char); // putchar(res_char);
+		else
+			printf("%c", ori_char); // putchar(ori_char);
+	}
+
+	return 0;
 }
